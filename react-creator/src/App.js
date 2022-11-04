64b12131afc91f170b60016f5b8edbdce8ef538c
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2022-11-04 15:17:27 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   react-creator/src/App.js

--- react-creator/src/App.js
+++ react-creator/src/App.js
@@ -1,46 +1,13 @@
-import { Button, Box } from "@chakra-ui/react";
-import classes from "./app.module.css";
-import { useState } from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import React from 'react'
 
 const App = () => {
-  const [clicked, setClicked] = useState(false);
-  const [items, setItems] = useState({
-    name: "Karthikeya",
-    year: 102,
-  });
-  const clickHandler = async () => {
-    const response = await fetch(
-      "https://demo1-6b4f0-default-rtdb.asia-southeast1.firebasedatabase.app/items.json"
-    );
-    const data = await response.json();
-    setItems(data);
-    setClicked(!clicked);
-  };
   return (
-    <ChakraProvider>
-      <div className={classes.main}>
-        <Button color="blue" bg="black" onClick={clickHandler}>
-          click me
-        </Button>
-        <Box border="2px" bg="black" h="500px">
-          {clicked && (
-            <Box
-              h="100px"
-              w="100px"
-              bg="white"
-              margin="4px"
-              p="4px"
-              border="4px"
-              borderColor="blue"
-              alignItems="center"
-            >
-              {items.name} <br />{items.data}
-            </Box>
-          )}
-        </Box>
-      </div>
-    </ChakraProvider>
-  );
-};
-export default App;
+    <React.Fragment>
+      <Route >
+        
+      </Route>
+    </React.Fragment>
+  )
+}
+
+export default App
