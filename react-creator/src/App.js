41bd12ecc70f835e47848f0e537f6e521dd7c514
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2022-11-04 15:49:58 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   react-creator/src/App.js
    	new file:   react-creator/src/pages/About.js

--- react-creator/src/App.js
+++ react-creator/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Route,Routes } from "react-router-dom";
 import Home from "./pages/Home"
+import About from "./pages/About"
 
 const App = () => {
   return (
     <React.Fragment>
-      Nothing here
+      <div >
+        Nothing here
+      </div>
       <Routes>
-        <Route path="/home" element={<Home />}>
-        </Route>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />}/>
       </Routes>
     </React.Fragment>
   );
