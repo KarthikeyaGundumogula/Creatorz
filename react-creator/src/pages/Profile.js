c25adb63d970575f1f61316f68352ca06737950e
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2022-11-07 07:00:01 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   react-creator/src/pages/Profile.js

--- react-creator/src/pages/Profile.js
+++ react-creator/src/pages/Profile.js
@@ -1,11 +1,41 @@
-import {Fragment} from 'react'
+import { Fragment } from "react";
+import { Text, Center, Box, Button, Image } from "@chakra-ui/react";
 
 const Profile = (props) => {
   return (
     <Fragment>
-      <h2 style={border}>Karthikeya</h2>
+      <Button borderRight="50px" borderRadius="12px" margin="2px">
+        .connect
+      </Button>
+      <Center>
+        <Box margin="20px" borderWidth="2px" w="480px">
+          <Center>
+            <Text fontSize="6xl" borderTop="50px">
+              Dan Abramov
+            </Text>
+          </Center>
+          <Center>
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              borderColor="black.100"
+              alt="Dan Abramov"
+              fallbackSrc="https://via.placeholder.com/150"
+            />
+          </Center>
+          <Center>
+            <Text fontSize="2xl">Acc Balance</Text>
+          </Center>
+          <Center>
+            <Text fontSize="2xl">Ethereum</Text>
+          </Center>
+          <Center>
+            <Text fontSize="2xl">Native TOken</Text>
+          </Center>
+        </Box>
+      </Center>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
