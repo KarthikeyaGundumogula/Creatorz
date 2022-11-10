75d10be0f80535d3b47ee93a4753e056aa539843
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2022-11-10 09:21:47 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   react-creator/src/pages/NewToken.js

--- react-creator/src/pages/NewToken.js
+++ react-creator/src/pages/NewToken.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import {Center,Heading} from '@chakra-ui/react'
+import React from "react";
+import { Box,Heading, Input,Text, HStack,Button } from "@chakra-ui/react";
 
 const NewToken = () => {
   return (
-    <Center borderWidth={2} margin="10%">
-        <Heading>Get NewToken</Heading>
-    </Center>
+    <Box margin="17%" borderWidth={1} p={1}>
+      <Heading>Get New Token</Heading>
+      <Box margin={8}>
+        <HStack margin={1}>
+          <Text fontSize={24}>Name:</Text>
+          <Input />
+        </HStack>
+        <HStack margin={1}>
+          <Text fontSize={24}>Amount:</Text>
+          <Input />
+        </HStack>
+        <HStack margin={1}>
+          <Text fontSize={24}></Text>
+          <Input type='number'/>
+        </HStack>
+      </Box>
+    </Box>
   );
-}
+};
 
-export default NewToken
+export default NewToken;
