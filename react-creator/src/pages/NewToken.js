21d3df20a3a2f2b4529e0af9353c0c4d20951b47
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2022-11-10 08:51:44 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   react-creator/src/pages/NewToken.js

--- react-creator/src/pages/NewToken.js
+++ react-creator/src/pages/NewToken.js
@@ -4,7 +4,7 @@ import {Center,Heading} from '@chakra-ui/react'
 const NewToken = () => {
   return (
     <Center borderWidth={2} margin="10%">
-        
+        <Heading>Get NewToken</Heading>
     </Center>
   );
 }
