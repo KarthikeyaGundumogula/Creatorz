ebc408ac7faba5bca74cf0abe1e9f6614a98f1ae
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2022-11-13 16:11:01 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Hardhat/contracts/Creator.sol
    	new file:   Hardhat/contracts/NFT-MarketPlace.sol
    	modified:   Hardhat/package-lock.json
    	modified:   Hardhat/package.json

--- Hardhat/contracts/Creator.sol
+++ Hardhat/contracts/Creator.sol
@@ -1,6 +1,9 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: Unlicense
 
-contract Creator {
-    
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract Creator is ERC1155{
+    constructor
 }
